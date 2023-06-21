d34e4d5b04360ed8893249a99103dff77640b839
MATHEUS BECHER SCHNEIDER <matheus_b_schneider@estudante.sesisenai.org.br>
2023-06-21 16:54:57 -0300

    Commit Inicial

---
+++ logica/script.js
@@ -0,0 +1,123 @@
+
+//let idade
+
+//idade = Number(prompt("Digite sua idade"))
+
+//if(idade >= 18){
+    //alert("você pode entrar")
+//}else{
+   // alert("Tente novamente em alguns anos")
+    //alert("Cresce criança")
+    //alert("Volta para a floresta")
+//}
+
+
+// let senha
+
+// senha = Number(prompt("Digite a senha"))
+
+// if(senha == 1234){
+//     alert("você pode entrar")
+// }else{
+//     alert("senha incorreta")
+// }
+
+// let Numero
+
+// Numero1 = Number(prompt("Digite o numero"))
+// numero2 = Number(prompt("Digite o segundo numero"))
+// if(Numero1 > numero2){
+//     alert(Numero1)
+// }else{
+//     alert(numero2)
+// }
+
+// let Placa
+
+// Placa = Number(prompt("Digite o ultimo numero da p"))
+// if(placa == 0 || placa == 1){
+//     alert("segunda")
+// }
+
+// if(placa == 2 || placa == 3){
+//     alert("terça")
+// }
+
+// if(placa == 4 || placa == 5){
+//     alert("Quarta")
+// }
+
+// if(placa == 6 || placa == 7){
+//     alert("Quinta")
+// }
+
+// if(placa == 8 || placa == 9){
+//     alert("Sexta")
+// }
+
+// let palestra
+
+// palestra = number (prompt("em qual sala você deseja ir?"))
+
+// if (palestra == 1) {
+//     alert("Animaçôes com scratch as 19:00: sala 305 ")
+// }
+// if (palestra == 2) {
+//     alert ("Scratch para gamer ás 20:00: sala 512")
+// }
+// if(palestra == 3) {
+//     alert("JavaScript para leigos ás 19:00 sala 101")
+
+// }
+// if(palestra == 4){
+//     alert("Tópicos avançados de JavaScript ás 20:00: sala 305")
+//     if(palestra == 5)
+//     alert("Vida e carreira ás 21:00: auditório")
+// }
+
+// let média, nota1, nota2
+
+// nota1 = Number(prompt("digit a sua primeira nota"))
+// nota2 = Number(prompt("digte sua segunda nota"))
+// média = (nota1+nota2)/2
+
+// if(média > 7){
+//     alert("Aprovado")
+// }else{
+//     alert("Reprovado")
+// }
+// alert("suamédia final é: " + média)
+
+//let pontuação 
+
+// pontuação = Number(prompt("Digite a sua pontuação: "))
+// if(pontuação <= 10){
+//     alert("Deu ruim")
+// }
+// if(pontuação >10 && pontuação <=100){
+//     alert("Tá quase em")
+// }
+// if(pontuação >100 && pontuação <=200){
+//     alert("Supimpa")
+// }
+// if(pontuação >200){
+//     alert("MITOU!")
+// }
+
+// let dias, multa, VD, conta
+
+// dias = Number(prompt("Quantos dias você pretende ficar no albergue? "))
+// D = Number(0.25)
+// multa = Number(150)
+
+// if(dias <= 5){
+//    VD = (100*dias)  
+// }
+// if(dias >5 && dias <= 10){
+//     VD = (90*dias)
+// }
+// if(dias >10){
+//     VD = (80*dias)
+// }
+// conta = VD*0.75 + multa
+// alert("Conta: " + conta)
